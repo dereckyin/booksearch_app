ee651005e579794657523179322ea933fa6821b1
dereckyin <dereckyin@yahoo.com.tw>
2026-01-29 23:49:22 +0800

    for build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.booksearch_app"
+    namespace = "tw.taaze.booksearchApp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.booksearch_app"
+        applicationId = "tw.taaze.booksearchApp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
